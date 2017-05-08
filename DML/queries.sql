e6bb14820bed64a9f1d4281c68b1cc93342878d0
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-05-08 11:08:06 -0600

    added sample queries

--- DML/queries.sql
+++ DML/queries.sql
@@ -1,1 +1,16 @@
 \c sql_music
+
+-- get albums with songs
+SELECT albums.name AS album_name, songs.name AS song_name
+FROM songs
+JOIN albums
+ON albums.id = songs.album_id;
+
+-- get songs with artists
+
+SELECT songs.name AS song_name, artists.name AS artist_name
+FROM songs
+JOIN artists_songs
+ON artists_songs.song_id = songs.id
+JOIN artists
+ON artists.id = artists_songs.artist_id;
--- DML/seeds/songs.sql
+++ DML/seeds/songs.sql
@@ -6,7 +6,7 @@ TRUNCATE songs CASCADE;
 
 INSERT INTO songs (name, explicit_lyrics, album_id)
 VALUES
-('Poetic Justic', true, (SELECT id FROM albums WHERE name = 'Good Kid, M.A.A.D. City')),
+('Poetic Justice', true, (SELECT id FROM albums WHERE name = 'Good Kid, M.A.A.D. City')),
 ('Moment 4 Life', true, (SELECT id FROM albums WHERE name = 'Pink Friday')),
 ('Believe Me', true, (SELECT id FROM albums WHERE name = 'Tha Carter V')),
 ('Buried Alive', true, (SELECT id FROM albums WHERE name = 'Take Care'));
